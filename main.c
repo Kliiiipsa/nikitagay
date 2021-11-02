295091e5043f70be8cb6e74adc9e694a13bbd0e2
kotbkbahte <kot-b-kbahte@yandex.ru>
2021-11-02 15:45:19 +0300

    base refactoring

--- main.c
+++ main.c
@@ -7,111 +7,91 @@
 
 #define width 65
 #define height 25
+typedef struct {int x,y;int w;}TRacket;typedef struct { int x, y;}TBall;char mas[height][width+1];TRacket racket;TBall ball;void initball(){ball.x = 2;ball.y = 2;}void Putball(){mas[ball.y][ball.x] = '*';}
 
-  typedef struct {
-       int x,y;
-       int w;
-  } TRacket;
+void moveball(int x, int y)
+{
+    ball.x = x;
+    ball.y = y;
 
-  typedef struct {
-      int x, y;
+}
 
-  }TBall;
+void initRacket()
+{
+   racket.w = 7;
+   racket.x = (width +  racket.w) / 2;
+   racket.y = height - 1;
 
-char mas[height][width+1];
+}
 
-    TRacket racket;
-    TBall ball;
+void PutRacket()
+{
+   for(int i = racket.x ; i <racket.x + racket.w ; i++)
+    mas[racket.y][i] = '@';
 
-    void initball()
+}
 
-    {
-        ball.x = 2;
-        ball.y = 2;
-    }
-    void Putball()
-    {
-        mas[ball.y][ball.x] = '*';
-    }
-    void moveball(int x, int y)
-    {
-        ball.x = x;
-        ball.y = y;
+void def()
+{
 
-    }
 
-   void initRacket()
-   {
-       racket.w = 7;
-       racket.x = (width +  racket.w) / 2;
-       racket.y = height - 1;
+}
 
-   }
-
-   void PutRacket()
-   {
-       for(int i = racket.x ; i <racket.x + racket.w ; i++)
-        mas[racket.y][i] = '@';
-
-   }
 
 void init()
-
 {
     for(int i = 0 ; i < width; i++)
         mas[0][i] = '#';
-        mas[0][width] = '\0';
+    mas[0][width] = '\0';
 
-        strncpy(mas[1], mas[0], width+1);
-        for(int i = 1 ; i < width-1; i++)
+    strncpy(mas[1], mas[0], width+1);
+    for(int i = 1 ; i < width-1; i++)
         mas[1][i] = ' ';
 
-        for(int i = 1 ; i  < height; i++)
+    for(int i = 1 ; i  < height; i++)
         strncpy(mas[i], mas[1], width+1);
 
 
-        }
+}
 
-    void show()
+void show()
+{
+    for(int i = 0 ; i  < height; i++)
     {
-        for(int i = 0 ; i  < height; i++)
-        {
-
-
         printf("%s", mas[i]);
         if ( i <  height - 1)
         printf("\n");
 
     }
-    }
+}
 
-    void MoveRacket(int x)
-    {
-        racket.x = x;
-        if (racket.x < 1)
-            racket.x = 1;
-        if (racket.x + racket.w >= width)
-            racket.x = width - 1 - racket.w;
+void MoveRacket(int x)
+{
+    racket.x = x;
+    if (racket.x < 1)
+        racket.x = 1;
+    if (racket.x + racket.w >= width)
+        racket.x = width - 1 - racket.w;
+
+}
 
 
-
-    }
-   void setcur(int x, int y)
-   {
-       COORD coord;
-       coord.X = x;
-       coord.Y = y;
-       SetConsoleCursorPosition( GetStdHandle(STD_OUTPUT_HANDLE), coord);
-   }
+void setcur(int x, int y)
+{
+   COORD coord;
+   coord.X = x;
+   coord.Y = y;
+   SetConsoleCursorPosition( GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
 
 int main()
 {
     char c;
-        initRacket();
-        initball();
+    initRacket();
+    initball();
 
-         do{
-                setcur(0, 0);
+    do{
+        setcur(0, 0);
         init();
         PutRacket();
         Putball();
@@ -122,7 +102,7 @@ int main()
         if (GetKeyState('D') < 0) MoveRacket(racket.x + 1);
         Sleep(5);
 
-         }
-         while(GetKeyState(VK_ESCAPE) >= 0);
+    }
+    while(GetKeyState(VK_ESCAPE) >= 0);
     return 0;
 }
